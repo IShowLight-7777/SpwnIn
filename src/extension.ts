1fdfcda63cd10e393b54da8126cdd41500852c7d
Avaneesh Naik <avaneeshnaik800@gmail.com>
2021-08-27 20:37:40 +0530

    y e s

--- src/extension.ts
+++ src/extension.ts
@@ -14,13 +14,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('spwnin.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from spwnIn!');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
