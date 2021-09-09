0ac6aac8040a58bcb1d2d62f911e59b2874f63e8
Avaneesh Naik <avaneeshnaik2663@gmail.com>
2021-09-09 19:25:19 +0530

    half donut

--- src/extension.ts
+++ src/extension.ts
@@ -87,10 +87,51 @@ GROUP_ID.move(10, 10, 0.5) // moves the group 1 block up on the y axis and and 1
 			return vscode.window.showErrorMessage(`Failed to create "${file}" file. are you in a workspace or a folder?`);
 		}
 	});
+	let doughnut = vscode.commands.registerCommand('spwnin.doughnut', () => {
+		let donut = `
+			 extract $; let		
+		 v=[];extract obj_props
+	  p=3.14;h=100;d=sin;u=cos;for
+	i in..20..628{for j in..40..628{
+   }t=d(i/h);c=u(i/h);s=d(j/h;p=u(j/h
+  );x=3*c+p*c;y=3*t+p*t;g=?g;a=1/(s+5)
+ add(obj{1:725,X:615+40*a*(x*5),Y:415 +
+ 40*a*(y*5),57:g});v.push([x,y,s,g]);)}
+ r=(a,i){c,u(a*p/      180);s=d(a*p/180
+);n=i[0];m=i[1];        l=i[2];z=(-s)*n+
+}
+		`;
+
+		const folderPath = vscode!.workspace!.workspaceFolders![0].uri.fsPath; 
+		const file = 'donut.spwn';
+
+		if (fs.existsSync(folderPath)) {
+			let filepath = path.join(folderPath, file);
+
+			try {
+				let exists = fs.existsSync(filepath);
+				fs.writeFile(filepath, donut, err => {
+					if(err){
+						console.error(err);
+						return vscode.window.showErrorMessage(`Failed to ${exists ? 'edit' : 'create'} "${file}" file.`);
+					}
+					return vscode.window.showWarningMessage(
+						exists ? `FILE ALREADY EXISTS | edited "${file}" file.` : `created "${file}" file.`
+					);
+				});
+			} catch(err) {
+				console.error(err);
+			}
+					
+		} else {
+			return vscode.window.showErrorMessage(`Failed to create "${file}" file. are you in a workspace or a folder?`);
+		}
+	});
 	//push context
 	// so apprenlty you dont need this for making new commands
 	context.subscriptions.push(samplebobcode);
 	context.subscriptions.push(sampleontouch); // outdated version
+	context.subscriptions.push(doughnut);
 }
 
 export function deactivate() {
