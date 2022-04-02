c4fd80458353e2e9b9918a53c166d74fb85a9b5a
Avaneesh-coder <avaneeshnaik2663@gmail.com>
2022-04-02 20:18:31 +0530

    rework

--- client/out/extension.js
+++ client/out/extension.js
@@ -2,115 +2,45 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
 const path = require("path");
-const vsc = require("vscode");
+const vscode_1 = require("vscode");
 const node_1 = require("vscode-languageclient/node");
-let defaultClient;
-const clients = new Map();
-let _sortedWorkspaceFolders;
-function sortedWorkspaceFolders() {
-    if (_sortedWorkspaceFolders === void 0) {
-        _sortedWorkspaceFolders = vsc.workspace.workspaceFolders ? vsc.workspace.workspaceFolders.map(folder => {
-            let result = folder.uri.toString();
-            if (result.charAt(result.length - 1) !== '/') {
-                result = result + '/';
-            }
-            return result;
-        }).sort((a, b) => {
-            return a.length - b.length;
-        }) : [];
-    }
-    return _sortedWorkspaceFolders;
-}
-vsc.workspace.onDidChangeWorkspaceFolders(() => _sortedWorkspaceFolders = undefined);
-function getOuterMostWorkspaceFolder(folder) {
-    const sorted = sortedWorkspaceFolders();
-    for (const element of sorted) {
-        let uri = folder.uri.toString();
-        if (uri.charAt(uri.length - 1) !== '/') {
-            uri = uri + '/';
-        }
-        if (uri.startsWith(element)) {
-            return vsc.workspace.getWorkspaceFolder(vsc.Uri.parse(element));
-        }
-    }
-    return folder;
-}
+let client;
 function activate(context) {
-    const module = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
-    const outputChannel = vsc.window.createOutputChannel('lsp-multi-server-example');
-    function didOpenTextDocument(document) {
-        // We are only interested in language mode text
-        if (document.languageId !== "spwn" || (document.uri.scheme !== 'file' && document.uri.scheme !== 'untitled')) {
-            return;
+    // The server is implemented in node
+    const serverModule = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
+    // The debug options for the server
+    // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+    const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+    // If the extension is launched in debug mode then the debug server options are used
+    // Otherwise the run options are used
+    const serverOptions = {
+        run: { module: serverModule, transport: node_1.TransportKind.ipc },
+        debug: {
+            module: serverModule,
+            transport: node_1.TransportKind.ipc,
+            options: debugOptions
         }
-        const uri = document.uri;
-        // Untitled files go to a default client.
-        if (uri.scheme === 'untitled' && !defaultClient) {
-            const debugOptions = { execArgv: ["--nolazy", "--inspect=6010"] };
-            const serverOptions = {
-                run: { module, transport: node_1.TransportKind.ipc },
-                debug: { module, transport: node_1.TransportKind.ipc, options: debugOptions }
-            };
-            const clientOptions = {
-                documentSelector: [
-                    { scheme: 'untitled', language: 'spwn' }
-                ],
-                diagnosticCollectionName: 'lsp-multi-server-example',
-                outputChannel: outputChannel
-            };
-            defaultClient = new node_1.LanguageClient('lsp-multi-server-example', 'LSP Multi Server Example', serverOptions, clientOptions);
-            defaultClient.start();
-            return;
+    };
+    // Options to control the language client
+    const clientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [{ scheme: 'file', language: 'spwn' }],
+        synchronize: {
+            // Notify the server about file changes to '.clientrc files contained in the workspace
+            fileEvents: vscode_1.workspace.createFileSystemWatcher('**/.clientrc')
         }
-        let folder = vsc.workspace.getWorkspaceFolder(uri);
-        // Files outside a folder can't be handled. This might depend on the language.
-        // Single file languages like JSON might handle files outside the workspace folders.
-        if (!folder) {
-            return;
-        }
-        // If we have nested workspace folders we only start a server on the outer most workspace folder.
-        folder = getOuterMostWorkspaceFolder(folder);
-        if (!clients.has(folder.uri.toString())) {
-            const debugOptions = { execArgv: ["--nolazy", `--inspect=${6011 + clients.size}`] };
-            const serverOptions = {
-                run: { module, transport: node_1.TransportKind.ipc },
-                debug: { module, transport: node_1.TransportKind.ipc, options: debugOptions }
-            };
-            const clientOptions = {
-                documentSelector: [
-                    { scheme: 'file', language: 'spwn', pattern: `${folder.uri.fsPath}/**/*` }
-                ],
-                diagnosticCollectionName: 'lsp-multi-server-example',
-                workspaceFolder: folder,
-                outputChannel: outputChannel
-            };
-            const client = new node_1.LanguageClient('lsp-multi-server-example', 'LSP Multi Server Example', serverOptions, clientOptions);
-            client.start();
-            clients.set(folder.uri.toString(), client);
-        }
-    }
-    vsc.workspace.onDidOpenTextDocument(didOpenTextDocument);
-    vsc.workspace.textDocuments.forEach(didOpenTextDocument);
-    vsc.workspace.onDidChangeWorkspaceFolders((event) => {
-        for (const folder of event.removed) {
-            const client = clients.get(folder.uri.toString());
-            if (client) {
-                clients.delete(folder.uri.toString());
-                client.stop();
-            }
-        }
-    });
+    };
+    // Create the language client and start the client.
+    client = new node_1.LanguageClient('languageServerExample', 'Language Server Example', serverOptions, clientOptions);
+    // Start the client. This will also launch the server
+    client.start();
 }
 exports.activate = activate;
 function deactivate() {
-    const promises = [];
-    if (defaultClient) {
-        promises.push(defaultClient.stop());
+    if (!client) {
+        return undefined;
     }
-    for (const client of clients.values()) {
-        promises.push(client.stop());
-    }
-    return Promise.all(promises).then(() => undefined);
+    return client.stop();
 }
 exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,127 +1,58 @@
-
 import * as path from 'path';
-import * as vsc from 'vscode';
-import * as fs from 'fs';
+import { workspace, ExtensionContext } from 'vscode';
 
 import {
-	LanguageClient, LanguageClientOptions, TransportKind
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind
 } from 'vscode-languageclient/node';
 
-let defaultClient: LanguageClient;
-const clients: Map<string, LanguageClient> = new Map();
+let client: LanguageClient;
 
-let _sortedWorkspaceFolders: string[] | undefined;
-function sortedWorkspaceFolders(): string[] {
-	if (_sortedWorkspaceFolders === void 0) {
-		_sortedWorkspaceFolders = vsc.workspace.workspaceFolders ? vsc.workspace.workspaceFolders.map(folder => {
-			let result = folder.uri.toString();
-			if (result.charAt(result.length - 1) !== '/') {
-				result = result + '/';
-			}
-			return result;
-		}).sort(
-			(a, b) => {
-				return a.length - b.length;
-			}
-		) : [];
-	}
-	return _sortedWorkspaceFolders;
-}
-vsc.workspace.onDidChangeWorkspaceFolders(() => _sortedWorkspaceFolders = undefined);
+export function activate(context: ExtensionContext) {
+  // The server is implemented in node
+  const serverModule = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
+  // The debug options for the server
+  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+  const debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
 
-function getOuterMostWorkspaceFolder(folder: vsc.WorkspaceFolder): vsc.WorkspaceFolder {
-	const sorted = sortedWorkspaceFolders();
-	for (const element of sorted) {
-		let uri = folder.uri.toString();
-		if (uri.charAt(uri.length - 1) !== '/') {
-			uri = uri + '/';
-		}
-		if (uri.startsWith(element)) {
-			return vsc.workspace.getWorkspaceFolder(vsc.Uri.parse(element))!;
-		}
-	}
-	return folder;
-}
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: debugOptions
+    }
+  };
 
-export function activate(context: vsc.ExtensionContext) {
+  // Options to control the language client
+  const clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [{ scheme: 'file', language: 'spwn' }],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+    }
+  };
 
-	const module = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
-	const outputChannel: vsc.OutputChannel = vsc.window.createOutputChannel('lsp-multi-server-example');
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    'languageServerExample',
+    'Language Server Example',
+    serverOptions,
+    clientOptions
+  );
 
-	function didOpenTextDocument(document: vsc.TextDocument): void {
-		// We are only interested in language mode text
-		if (document.languageId !=="spwn" || (document.uri.scheme !== 'file' && document.uri.scheme !== 'untitled')) {
-			return;
-		}
-
-		const uri = document.uri;
-		// Untitled files go to a default client.
-		if (uri.scheme === 'untitled' && !defaultClient) {
-			const debugOptions = { execArgv: ["--nolazy", "--inspect=6010"] };
-			const serverOptions = {
-				run: { module, transport: TransportKind.ipc },
-				debug: { module, transport: TransportKind.ipc, options: debugOptions}
-			};
-			const clientOptions: LanguageClientOptions = {
-				documentSelector: [
-					{ scheme: 'untitled', language: 'spwn' }
-				],
-				diagnosticCollectionName: 'lsp-multi-server-example',
-				outputChannel: outputChannel
-			};
-			defaultClient = new LanguageClient('lsp-multi-server-example', 'LSP Multi Server Example', serverOptions, clientOptions);
-			defaultClient.start();
-			return;
-		}
-		let folder = vsc.workspace.getWorkspaceFolder(uri);
-		// Files outside a folder can't be handled. This might depend on the language.
-		// Single file languages like JSON might handle files outside the workspace folders.
-		if (!folder) {
-			return;
-		}
-		// If we have nested workspace folders we only start a server on the outer most workspace folder.
-		folder = getOuterMostWorkspaceFolder(folder);
-
-		if (!clients.has(folder.uri.toString())) {
-			const debugOptions = { execArgv: ["--nolazy", `--inspect=${6011 + clients.size}`] };
-			const serverOptions = {
-				run: { module, transport: TransportKind.ipc },
-				debug: { module, transport: TransportKind.ipc, options: debugOptions}
-			};
-			const clientOptions: LanguageClientOptions = {
-				documentSelector: [
-					{ scheme: 'file', language: 'spwn', pattern: `${folder.uri.fsPath}/**/*` }
-				],
-				diagnosticCollectionName: 'lsp-multi-server-example',
-				workspaceFolder: folder,
-				outputChannel: outputChannel
-			};
-			const client = new LanguageClient('lsp-multi-server-example', 'LSP Multi Server Example', serverOptions, clientOptions);
-			client.start();
-			clients.set(folder.uri.toString(), client);
-		}
-	}
-
-	vsc.workspace.onDidOpenTextDocument(didOpenTextDocument);
-	vsc.workspace.textDocuments.forEach(didOpenTextDocument);
-	vsc.workspace.onDidChangeWorkspaceFolders((event) => {
-		for (const folder  of event.removed) {
-			const client = clients.get(folder.uri.toString());
-			if (client) {
-				clients.delete(folder.uri.toString());
-				client.stop();
-			}
-		}
-	});
+  // Start the client. This will also launch the server
+  client.start();
 }
 
-export function deactivate(): Thenable<void> {
-	const promises: Thenable<void>[] = [];
-	if (defaultClient) {
-		promises.push(defaultClient.stop());
-	}
-	for (const client of clients.values()) {
-		promises.push(client.stop());
-	}
-	return Promise.all(promises).then(() => undefined);
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
 }
